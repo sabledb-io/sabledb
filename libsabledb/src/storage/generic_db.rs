23dde338e4fef2c49467f8587a10872c8d8d3d27
0del <bany.y0599@gmail.com>
2024-04-09 22:58:25 +0700

    add more test

--- libsabledb/src/storage/generic_db.rs
+++ libsabledb/src/storage/generic_db.rs
@@ -64,9 +64,7 @@ impl<'a> GenericDb<'a> {
         let Some((_, common_md)) = self.get_internal(user_key)? else {
             return Ok(None);
         };
-        let exp = common_md.expiration();
-        println!("{}", exp.);
-        Ok(Some(exp))
+        Ok(Some(common_md.expiration().clone()))
     }
 
     /// Update the expiration properties of `user_key`
