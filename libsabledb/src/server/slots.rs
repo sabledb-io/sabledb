98a543a3bbc9e1083cbbbca5aa6ac5ebe1f72d8a
sabledb <sabledb.database@gmail.com>
2024-12-08 23:42:53 +0200

    Implemented `SlotBitmap::from_str` and `SlotBitmap::to_string()`
    Signed-off-by: sabledb <sabledb.database@gmail.com>

--- libsabledb/src/server/slots.rs
+++ libsabledb/src/server/slots.rs
@@ -1,7 +1,9 @@
 use crate::utils::SLOT_SIZE;
 use crate::SableError;
+use std::str::FromStr;
+use std::string::ToString;
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, PartialEq)]
 pub struct SlotBitmap {
     bits: Vec<u128>,
 }
@@ -14,6 +16,9 @@ impl Default for SlotBitmap {
     }
 }
 
+/// This provides a mapping of slots owned by this instance of SableDB.
+/// Each slot uses a single bit to mark whether it is owned by this instance or not.
+/// We use an array of 128 `u128` primitives to keep track of the slots (total of 16K memory)
 impl SlotBitmap {
     /// Set or clear `slot` in this slots set
     pub fn set(&mut self, slot: u16, b: bool) -> Result<(), SableError> {
@@ -77,6 +82,124 @@ impl SlotBitmap {
     }
 }
 
+impl FromStr for SlotBitmap {
+    type Err = SableError;
+    /// Parse `s` into `SlotBitmap`
+    ///
+    /// The expected format is a comma separated list of slots. Range of slots
+    /// can be represented by `start - end` pairs.
+    /// For example, in order to represent slots from `0` - `10000` and also include the slot `15000`
+    /// we can use the following string format:
+    /// `0-10000,15000`
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut slots = SlotBitmap::default();
+        let tokens: Vec<&str> = s.split(',').map(|s| s.trim()).collect();
+        let into_sable_err =
+            |e| SableError::OtherError(format!("Failed to parse string into u16. {}", e));
+        for token in &tokens {
+            if let Some(hyphen_pos) = token.find('-') {
+                let first = token[0..hyphen_pos]
+                    .trim()
+                    .parse::<u16>()
+                    .map_err(into_sable_err)?;
+                let second = token[hyphen_pos + 1..] // + 1 : skip the hyphen
+                    .trim()
+                    .parse::<u16>()
+                    .map_err(into_sable_err)?;
+                if second > first {
+                    for slot in first..=second {
+                        slots.set(slot, true)?;
+                    }
+                }
+            } else {
+                // single token
+                let slot = token.parse::<u16>().map_err(into_sable_err)?;
+                slots.set(slot, true)?;
+            }
+        }
+        Ok(slots)
+    }
+}
+
+#[derive(Default)]
+struct SlotRange {
+    start: Option<u16>,
+    end: Option<u16>,
+    length: u16,
+}
+
+impl SlotRange {
+    pub fn try_add(&mut self, slot: u16) -> bool {
+        let (Some(_), Some(end)) = (&mut self.start, &mut self.end) else {
+            self.start = Some(slot);
+            self.end = Some(slot);
+            self.length = 1;
+            return true;
+        };
+
+        if end.saturating_add(1) == slot {
+            *end = slot;
+            self.length = self.length.saturating_add(1);
+            true
+        } else {
+            false
+        }
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.len() == 0
+    }
+
+    pub fn len(&self) -> u16 {
+        self.length
+    }
+}
+
+impl ToString for SlotRange {
+    fn to_string(&self) -> String {
+        let (Some(start), Some(end)) = (&self.start, &self.end) else {
+            return String::default();
+        };
+
+        if self.length == 1 {
+            format!("{}", start)
+        } else {
+            format!("{}-{}", start, end)
+        }
+    }
+}
+
+impl ToString for SlotBitmap {
+    fn to_string(&self) -> String {
+        let mut ranges = Vec::<SlotRange>::default();
+
+        let mut slot_range = SlotRange::default();
+        for slot in 0..SLOT_SIZE {
+            if self.is_set(slot).expect("slot not in range") {
+                if slot_range.try_add(slot) {
+                    continue;
+                } else {
+                    // close this range and start a new one
+                    ranges.push(slot_range);
+                    slot_range = SlotRange::default();
+                    slot_range.try_add(slot); // should not fail
+                }
+            }
+        }
+
+        if !slot_range.is_empty() {
+            ranges.push(slot_range);
+        }
+
+        // Join the vector into a string
+        ranges
+            .iter()
+            .map(|r| r.to_string())
+            .collect::<Vec<String>>()
+            .join(",")
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -93,5 +216,24 @@ mod tests {
         assert!(!slots.is_set(13456).unwrap());
         assert!(slots.set(13456, true).is_ok());
         assert!(slots.is_set(13456).unwrap());
+        assert!(slots.set(13456, false).is_ok());
+        assert!(!slots.is_set(13456).unwrap());
+    }
+
+    #[test]
+    fn test_slot_bitmap_to_string() {
+        let mut slots = SlotBitmap::default();
+        slots.set(0, true).unwrap();
+        slots.set(2, true).unwrap();
+        slots.set(3, true).unwrap();
+        slots.set(5, true).unwrap();
+        slots.set(8000, true).unwrap();
+        slots.set(8001, true).unwrap();
+        slots.set(8003, true).unwrap();
+
+        assert_eq!("0,2-3,5,8000-8001,8003", slots.to_string());
+
+        let slots2 = SlotBitmap::from_str("0,2-3,5,8000-8001,8003").unwrap();
+        assert_eq!(slots, slots2);
     }
 }
